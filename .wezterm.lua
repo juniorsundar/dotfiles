0e734ba74005ff42377c8e0999d27a3aadaae934
Junior Sundar <juniorsundar@gmail.com>
2024-01-26 14:34:39 +0400

    changed wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,9 +14,58 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+
+config.colors = {
+    foreground = "#c0caf5",
+    background = "#1a1b26",
+    cursor_bg = "#c0caf5",
+    cursor_border = "#c0caf5",
+    cursor_fg = "#1a1b26",
+    selection_bg = "#283457",
+    selection_fg = "#c0caf5",
+    split = "#7aa2f7",
+    compose_cursor = "#ff9e64",
+    scrollbar_thumb = "#292e42",
+
+    ansi = { "#15161e", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#a9b1d6" },
+    brights = { "#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#c0caf5" },
+}
+
+config.colors.tab_bar = {
+    inactive_tab_edge = "#16161e",
+    background = "#1a1b26",
+}
+
+config.colors.tab_bar.active_tab = {
+    fg_color = "#16161e",
+    bg_color = "#7aa2f7",
+}
+
+config.colors.tab_bar.inactive_tab = {
+    fg_color = "#545c7e",
+    bg_color = "#292e42",
+}
+
+config.colors.tab_bar.inactive_tab_hover = {
+    fg_color = "#7aa2f7",
+    bg_color = "#292e42",
+    -- # intensity = "Bold"
+}
+
+config.colors.tab_bar.new_tab_hover = {
+    fg_color = "#7aa2f7",
+    bg_color = "#1a1b26",
+    intensity = "Bold",
+}
+
+config.colors.tab_bar.new_tab = {
+    fg_color = "#7aa2f7",
+    bg_color = "#1a1b26",
+}
+
 config.force_reverse_video_cursor = true
 
-config.color_scheme = "Catppuccin Mocha"
+config.font_size = 10
 config.hide_tab_bar_if_only_one_tab = true
 -- Disable font ligatures
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
