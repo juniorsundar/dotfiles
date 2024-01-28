18f42abb9aec33d6ea134b575886c372e850055e
Junior Sundar <juniorsundar@gmail.com>
2024-01-28 23:19:05 +0400

    Headlines are highlighted

--- .config/nvim/lua/plugins/nvim-neorg.lua
+++ .config/nvim/lua/plugins/nvim-neorg.lua
@@ -1,31 +1,41 @@
 return {
-	"nvim-neorg/neorg",
-	build = ":Neorg sync-parsers",
-	-- tag = "*",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		vim.opt.conceallevel = 2
-		require("neorg").setup({
-			load = {
-				["core.defaults"] = {}, -- Loads default behaviour
-				["core.concealer"] = {
-					config = {
-						icon_preset = "diamond",
-					},
-				}, -- Adds pretty icons to your documents
-				["core.dirman"] = { -- Manages Neorg workspaces
-					config = {
-						workspaces = {
-							notes = "~/neorg/notes",
-							journal = "~/neorg/journal",
-						    books = "~/neorg/books"
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        -- tag = "*",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            vim.opt.conceallevel = 2
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.concealer"] = {
+                        config = {
+                            icon_preset = "diamond",
                         },
-					},
-				},
-				["core.export"] = {},
-				["core.export.markdown"] = {},
-                ["core.summary"] = {},
-			},
-		})
-	end,
+                    },   -- Adds pretty icons to your documents
+                    ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/neorg/notes",
+                                journal = "~/neorg/journal",
+                                books = "~/neorg/books",
+                            },
+                        },
+                    },
+                    ["core.export"] = {},
+                    ["core.export.markdown"] = {},
+                    ["core.summary"] = {},
+                },
+            })
+        end,
+    },
+    {
+        "lukas-reineke/headlines.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        opts = {},
+        config = function()
+            require("headlines").setup {}
+        end
+    },
 }
