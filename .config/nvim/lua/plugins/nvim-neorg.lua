ec2c7374bc8e9c111de3bb1690dd8bea979a60af
Junior Sundar <junior.sundar@unikie.com>
2024-01-31 16:32:08 +0400

    Cleaning stuff

--- .config/nvim/lua/plugins/nvim-neorg.lua
+++ .config/nvim/lua/plugins/nvim-neorg.lua
@@ -1,42 +1,78 @@
 return {
-    {
-        "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
-        -- tag = "*",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            vim.opt.conceallevel = 2
-            require("neorg").setup({
-                load = {
-                    ["core.defaults"] = {}, -- Loads default behaviour
-                    ["core.concealer"] = {
-                        config = {
-                            icon_preset = "diamond",
-                        },
-                    },   -- Adds pretty icons to your documents
-                    ["core.dirman"] = { -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                default = "~/neorg",
-                                notes = "~/neorg/notes",
-                                books = "~/neorg/books",
-                                journal = "~/neorg/journal"
-                            },
-                        },
-                    },
-                    ["core.export"] = {},
-                    ["core.export.markdown"] = {},
-                    ["core.summary"] = {},
-                },
-            })
-        end,
-    },
-    -- {
-    --     "lukas-reineke/headlines.nvim",
-    --     dependencies = "nvim-treesitter/nvim-treesitter",
-    --     opts = {},
-    --     config = function()
-    --         require("headlines").setup {}
-    --     end
-    -- },
+	{
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		-- tag = "*",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "norg",
+				callback = function()
+					vim.opt_local.conceallevel = 2
+				end,
+			})
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.esupports.indent"] = {
+						config = {
+							-- indents = {
+							-- 	["heading2"] = {
+							-- 		indent = 3,
+							-- 	},
+							-- 	["heading3"] = {
+							-- 		indent = 6,
+							-- 	},
+							-- 	["heading4"] = {
+							-- 		indent = 9,
+							-- 	},
+							-- 	["heading5"] = {
+							-- 		indent = 12,
+							-- 	},
+							-- },
+							tweaks = {
+								unordered_list1 = 0,
+								unordered_list2 = 3,
+								unordered_list3 = 6,
+								unordered_list4 = 12,
+								unordered_list5 = 15,
+								ordered_list1 = 0,
+								ordered_list2 = 3,
+								ordered_list3 = 6,
+								ordered_list4 = 12,
+								ordered_list5 = 15,
+							},
+						},
+					},
+					["core.concealer"] = {
+						config = {
+							icon_preset = "diamond",
+							icons = { list = { icons = { "󰧞", "", "", "󰝤", "", "" } } },
+						},
+					}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								default = "~/neorg",
+								notes = "~/neorg/notes",
+								books = "~/neorg/books",
+								journal = "~/neorg/journal",
+							},
+						},
+					},
+					["core.export"] = {},
+					["core.export.markdown"] = {},
+					["core.summary"] = {},
+				},
+			})
+		end,
+	},
+	-- {
+	--     "lukas-reineke/headlines.nvim",
+	--     dependencies = "nvim-treesitter/nvim-treesitter",
+	--     opts = {},
+	--     config = function()
+	--         require("headlines").setup {}
+	--     end
+	-- },
 }
--- .config/nvim/lua/plugins/quality-of-life.lua
+++ .config/nvim/lua/plugins/quality-of-life.lua
@@ -1,49 +1,49 @@
 return {
 	{
-			"echasnovski/mini.comment",
-			version = false,
-			config = function()
-				require("mini.comment").setup({
-					-- Options which control module behavior
-					options = {
-						-- Function to compute custom 'commentstring' (optional)
-						custom_commentstring = nil,
+		"echasnovski/mini.comment",
+		version = false,
+		config = function()
+			require("mini.comment").setup({
+				-- Options which control module behavior
+				options = {
+					-- Function to compute custom 'commentstring' (optional)
+					custom_commentstring = nil,
 
-						-- Whether to ignore blank lines
-						ignore_blank_line = false,
+					-- Whether to ignore blank lines
+					ignore_blank_line = false,
 
-						-- Whether to recognize as comment only lines without indent
-						start_of_line = false,
+					-- Whether to recognize as comment only lines without indent
+					start_of_line = false,
 
-						-- Whether to ensure single space pad for comment parts
-						pad_comment_parts = true,
-					},
+					-- Whether to ensure single space pad for comment parts
+					pad_comment_parts = true,
+				},
 
-					-- Module mappings. Use `''` (empty string) to disable one.
-					mappings = {
-						-- Toggle comment (like `gcip` - comment inner paragraph) for both
-						-- Normal and Visual modes
-						comment = "<C-c>",
+				-- Module mappings. Use `''` (empty string) to disable one.
+				mappings = {
+					-- Toggle comment (like `gcip` - comment inner paragraph) for both
+					-- Normal and Visual modes
+					comment = "<C-c>",
 
-						-- Toggle comment on current line
-						comment_line = "<C-c>c",
+					-- Toggle comment on current line
+					comment_line = "<C-c>c",
 
-						-- Toggle comment on visual selection
-						comment_visual = "<C-c>",
+					-- Toggle comment on visual selection
+					comment_visual = "<C-c>",
 
-						-- Define 'comment' textobject (like `dgc` - delete whole comment block)
-						textobject = "<C-c>",
-					},
+					-- Define 'comment' textobject (like `dgc` - delete whole comment block)
+					textobject = "<C-c>",
+				},
 
-					-- Hook functions to be executed at certain stage of commenting
-					hooks = {
-						-- Before successful commenting. Does nothing by default.
-						pre = function() end,
-						-- After successful commenting. Does nothing by default.
-						post = function() end,
-					},
-				})
-			end,
+				-- Hook functions to be executed at certain stage of commenting
+				hooks = {
+					-- Before successful commenting. Does nothing by default.
+					pre = function() end,
+					-- After successful commenting. Does nothing by default.
+					post = function() end,
+				},
+			})
+		end,
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
@@ -52,7 +52,7 @@ return {
 				char = "│",
 				tab_char = "│",
 			},
-			scope = { enabled = true },
+			scope = { enabled = false },
 			exclude = {
 				filetypes = {
 					"help",
@@ -66,13 +66,11 @@ return {
 					"notify",
 					"toggleterm",
 					"lazyterm",
+					"norg",
 				},
 			},
 		},
 		main = "ibl",
-		config = function()
-			require("ibl").setup()
-		end,
 	},
 	{
 		"chrisgrieser/nvim-recorder",
