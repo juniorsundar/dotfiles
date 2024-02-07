2b5b34b547118b09db5bf41e080a543d31e0854e
Junior Sundar <juniorsundar@gmail.com>
2024-02-07 19:50:27 +0400

    Clean up

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,7 +14,7 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.font = wezterm.font('MesloLGS Nerd Font Mono', { weight = 'Regular' })
+config.font = wezterm.font('MesloLGS Nerd Font', { weight = 'Regular' })
 config.font_size = 11
 
 config.colors = {
