74fd7f02ed388cb78687cb7b7f12f12dc8096ee2
juniorsundar-tii <junior.sundar@tii.ae>
2024-02-22 21:40:02 +0400

    Changed the navigation modules and comment

--- .config/nvim/lua/plugins/quality-of-life.lua
+++ .config/nvim/lua/plugins/quality-of-life.lua
@@ -1,89 +1,161 @@
 return {
-	{
-		"echasnovski/mini.comment",
-		version = false,
-		config = function()
-			require("mini.comment").setup({
-				-- Options which control module behavior
-				options = {
-					-- Function to compute custom 'commentstring' (optional)
-					custom_commentstring = nil,
-
-					-- Whether to ignore blank lines
-					ignore_blank_line = false,
-
-					-- Whether to recognize as comment only lines without indent
-					start_of_line = false,
-
-					-- Whether to ensure single space pad for comment parts
-					pad_comment_parts = true,
-				},
-
-				-- Module mappings. Use `''` (empty string) to disable one.
-				mappings = {
-					-- Toggle comment (like `gcip` - comment inner paragraph) for both
-					-- Normal and Visual modes
-					comment = "<C-c>",
-
-					-- Toggle comment on current line
-					comment_line = "<C-c>c",
-
-					-- Toggle comment on visual selection
-					comment_visual = "<C-c>",
-
-					-- Define 'comment' textobject (like `dgc` - delete whole comment block)
-					textobject = "<C-c>",
-				},
-
-				-- Hook functions to be executed at certain stage of commenting
-				hooks = {
-					-- Before successful commenting. Does nothing by default.
-					pre = function() end,
-					-- After successful commenting. Does nothing by default.
-					post = function() end,
-				},
-			})
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		opts = {
-			indent = {
-				char = "│",
-				tab_char = "│",
-			},
-			scope = { enabled = false },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-					"norg",
-				},
-			},
-		},
-		main = "ibl",
-	},
-	{
-		"chrisgrieser/nvim-recorder",
-		-- dependencies = "rcarriga/nvim-notify", -- optional
-		opts = {}, -- required even with default settings, since it calls `setup()`
-	},
-	{
-		"ggandor/leap.nvim",
-		dependencies = {
-			"tpope/vim-repeat",
-		},
-		config = function()
-			require("leap").create_default_mappings()
-		end,
-	},
+    {
+        "numToStr/Comment.nvim",
+        opts = {
+            ---Add a space b/w comment and the line
+            -- padding = true,
+            ---Whether the cursor should stay at its position
+            sticky = true,
+            ---Lines to be ignored while (un)comment
+            ignore = nil,
+            ---LHS of toggle mappings in NORMAL mode
+            toggler = {
+                ---Line-comment toggle keymap
+                line = "gcc",
+                ---Block-comment toggle keymap
+                block = "gBc",
+            },
+            ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+            opleader = {
+                ---Line-comment keymap
+                line = "gc",
+                ---Block-comment keymap
+                block = "gB",
+            },
+            ---LHS of extra mappings
+            extra = {
+                ---Add comment on the line above
+                above = "gcO",
+                ---Add comment on the line below
+                below = "gco",
+                ---Add comment at the end of line
+                eol = "gcA",
+            },
+            ---Enable keybindings
+            ---NOTE: If given `false` then the plugin won't create any mappings
+            mappings = {
+                ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+                basic = true,
+                ---Extra mapping; `gco`, `gcO`, `gcA`
+                extra = true,
+            },
+            ---Function to call before (un)comment
+            pre_hook = nil,
+            ---Function to call after (un)comment
+            post_hook = nil,
+        },
+        lazy = false,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = {
+            indent = {
+                char = "│",
+                tab_char = "│",
+            },
+            scope = { enabled = false },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                    "norg",
+                },
+            },
+        },
+        main = "ibl",
+    },
+    {
+        "chrisgrieser/nvim-recorder",
+        -- dependencies = "rcarriga/nvim-notify", -- optional
+        opts = {}, -- required even with default settings, since it calls `setup()`
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    },
+    {
+        "folke/zen-mode.nvim",
+        opts = {
+            window = {
+                backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+                -- height and width can be:
+                -- * an absolute number of cells when > 1
+                -- * a percentage of the width / height of the editor when <= 1
+                -- * a function that returns the width or the height
+                width = 120, -- width of the Zen window
+                height = 1, -- height of the Zen window
+                -- by default, no options are changed for the Zen window
+                -- uncomment any of the options below, or add other vim.wo options you want to apply
+                options = {
+                    -- signcolumn = "no", -- disable signcolumn
+                    -- number = false, -- disable number column
+                    -- relativenumber = false, -- disable relative numbers
+                    -- cursorline = false, -- disable cursorline
+                    -- cursorcolumn = false, -- disable cursor column
+                    -- foldcolumn = "0", -- disable fold column
+                    -- list = false, -- disable whitespace characters
+                },
+            },
+            plugins = {
+                -- disable some global vim options (vim.o...)
+                -- comment the lines to not apply the options
+                options = {
+                    enabled = true,
+                    ruler = false, -- disables the ruler text in the cmd line area
+                    showcmd = false, -- disables the command in the last line of the screen
+                    -- you may turn on/off statusline in zen mode by setting 'laststatus'
+                    -- statusline will be shown only if 'laststatus' == 3
+                    laststatus = 0, -- turn off the statusline in zen mode
+                },
+                twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+                gitsigns = { enabled = false }, -- disables git signs
+                tmux = { enabled = false }, -- disables the tmux statusline
+                -- this will change the font size on kitty when in zen mode
+                -- to make this work, you need to set the following kitty options:
+                -- - allow_remote_control socket-only
+                -- - listen_on unix:/tmp/kitty
+                kitty = {
+                    enabled = false,
+                    font = "+4", -- font size increment
+                },
+                -- this will change the font size on alacritty when in zen mode
+                -- requires  Alacritty Version 0.10.0 or higher
+                -- uses `alacritty msg` subcommand to change font size
+                alacritty = {
+                    enabled = false,
+                    font = "14", -- font size
+                },
+                -- this will change the font size on wezterm when in zen mode
+                -- See alse also the Plugins/Wezterm section in this projects README
+                wezterm = {
+                    enabled = false,
+                    -- can be either an absolute font size or the number of incremental steps
+                    font = "+4", -- (10% increase per step)
+                },
+            },
+            -- callback where you can add custom code when the Zen window opens
+            on_open = function(win) end,
+            -- callback where you can add custom code when the Zen window closes
+            on_close = function() end,
+        },
+    },
 }
