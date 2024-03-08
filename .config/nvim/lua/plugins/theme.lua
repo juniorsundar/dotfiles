16a6a7dbf47dea5721ad77cfa1f1d1efa3892146
Junior Sundar <junior.sundar@tii.ae>
2024-03-08 21:38:51 +0400

    Backup: 20240308:2138
    Former-commit-id: b16d789a78c8ed7499da0c4949a9eadd6ce7aade

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,55 +1,64 @@
 return {
-    {
-        "rebelot/kanagawa.nvim",
-        lazy = true,
-        priority = 1000,
-        config = function()
-            require("kanagawa").setup({
-                -- overrides = function(colors)
-                -- 	local theme = colors.theme
-                -- 	return {
-                -- 		-- NormalFloat = { bg = "none" },
-                -- 		FloatBorder = { bg = "none" },
-                -- 		FloatTitle = { bg = "none" },
-                -- 		NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                -- 	}
-                -- end,
-            })
-            -- vim.cmd.colorscheme("kanagawa-wave")
-        end,
-    },
-    { "rose-pine/neovim", name = "rose-pine" },
-    {
-        "folke/tokyonight.nvim",
-        lazy = true,
-        priority = 1000,
-        opts = {},
-        config = function()
-            require("tokyonight").setup({})
-        end,
-    },
-    {
-        "juniorsundar/nordic.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            local palette = require("nordic.colors")
-            require("nordic").setup({
-                telescope = {
-                    style = "classic",
-                },
-                leap = {
-                    dim_backdrop = true,
-                },
-                override = {
-                    WhichKeyBorder = { fg = palette.black1, bg = palette.black1 },
-                },
-            })
-            vim.opt.fillchars = { eob = " " }
-            vim.cmd.colorscheme("nordic")
-        end,
-    },
-    {
-        "nvim-tree/nvim-web-devicons",
-    },
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({
+				-- overrides = function(colors)
+				-- 	local theme = colors.theme
+				-- 	return {
+				-- 		-- NormalFloat = { bg = "none" },
+				-- 		FloatBorder = { bg = "none" },
+				-- 		FloatTitle = { bg = "none" },
+				-- 		NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+				-- 	}
+				-- end,
+			})
+			-- vim.cmd.colorscheme("kanagawa-wave")
+		end,
+	},
+	{
+		"juniorsundar/nightfox.nvim",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			require("nightfox").setup({
+				-- overrides = function(colors)
+				-- 	local theme = colors.theme
+				-- 	return {
+				-- 		-- NormalFloat = { bg = "none" },
+				-- 		FloatBorder = { bg = "none" },
+				-- 		FloatTitle = { bg = "none" },
+				-- 		NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+				-- 	}
+				-- end,
+			})
+			-- vim.cmd.colorscheme("kanagawa-wave")
+		end,
+	}, -- lazy
+	{
+		"juniorsundar/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local palette = require("nordic.colors")
+			require("nordic").setup({
+				telescope = {
+					style = "classic",
+				},
+				leap = {
+					dim_backdrop = true,
+				},
+				override = {
+					WhichKeyBorder = { fg = palette.black1, bg = palette.black1 },
+				},
+			})
+			vim.opt.fillchars = { eob = " " }
+			vim.cmd.colorscheme("nordic")
+		end,
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
 }
