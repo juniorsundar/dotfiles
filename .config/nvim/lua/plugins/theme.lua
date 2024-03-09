6fc7fe1c3a4529a3a5a3a281faf81350154ac797
Junior Sundar <juniorsundar@gmail.com>
2024-03-09 11:51:25 +0400

    Backup: 20240309:1151

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,24 +1,5 @@
 return {
     {
-        "rebelot/kanagawa.nvim",
-        lazy = true,
-        priority = 1000,
-        config = function()
-            require("kanagawa").setup({
-                -- overrides = function(colors)
-                -- 	local theme = colors.theme
-                -- 	return {
-                -- 		-- NormalFloat = { bg = "none" },
-                -- 		FloatBorder = { bg = "none" },
-                -- 		FloatTitle = { bg = "none" },
-                -- 		NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                -- 	}
-                -- end,
-            })
-            -- vim.cmd.colorscheme("kanagawa-wave")
-        end,
-    },
-    {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
@@ -49,47 +30,6 @@ return {
                 },
             })
             vim.cmd.colorscheme("catppuccin-frappe")
-        end,
-    },
-    { "rose-pine/neovim", lazy = true, name = "rose-pine" },
-    {
-        "juniorsundar/nightfox.nvim",
-        lazy = true,
-        priority = 1000,
-        config = function()
-            require("nightfox").setup({
-                -- overrides = function(colors)
-                -- 	local theme = colors.theme
-                -- 	return {
-                -- 		-- NormalFloat = { bg = "none" },
-                -- 		FloatBorder = { bg = "none" },
-                -- 		FloatTitle = { bg = "none" },
-                -- 		NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                -- 	}
-                -- end,
-            })
-            -- vim.cmd.colorscheme("kanagawa-wave")
-        end,
-    }, -- lazy
-    {
-        "juniorsundar/nordic.nvim",
-        lazy = true,
-        priority = 1000,
-        config = function()
-            local palette = require("nordic.colors")
-            require("nordic").setup({
-                telescope = {
-                    style = "classic",
-                },
-                leap = {
-                    dim_backdrop = true,
-                },
-                override = {
-                    WhichKeyBorder = { fg = palette.black1, bg = palette.black1 },
-                },
-            })
-            vim.opt.fillchars = { eob = " " }
-            -- vim.cmd.colorscheme("nordic")
         end,
     },
     {
