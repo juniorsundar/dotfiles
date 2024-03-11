8c4ce5a2c0294a13506b65fa0d850ac2e8170534
Junior Sundar <junior.sundar@tii.ae>
2024-03-11 16:09:03 +0400

    20240311:1609

--- .config/nvim/lua/plugins/quality-of-life.lua
+++ .config/nvim/lua/plugins/quality-of-life.lua
@@ -1,10 +1,5 @@
 return {
     {
-        "shortcuts/no-neck-pain.nvim",
-        version = "*",
-        cmd = "NoNeckPain",
-    },
-    {
         "numToStr/Comment.nvim",
         opts = {
             ---Add a space b/w comment and the line
@@ -96,15 +91,5 @@ return {
             { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<C-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
         },
-    },
-    {
-        "stevearc/oil.nvim",
-        cmd = "Oil",
-        opts = {},
-        -- Optional dependencies
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("oil").setup()
-        end,
     },
 }
