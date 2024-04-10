48fa05e749481b2d2d88156e3bfab5e963d3df3c
Junior Sundar <juniorsundar@gmail.com>
2024-04-10 23:55:03 +0400

    Backup: 20240410:2355
    Former-commit-id: 7ee08942428466422feda1b437c83bb78b321899

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,38 +1,35 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                custom_highlights = function(colors)
-                    return {
-                        WhichKeyBorder = { fg = colors.base },
-                        CmpBorder = { fg = colors.surface2 },
-                        Pmenu = { link = "NormalFloat" },
-                    }
-                end,
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    treesitter = true,
-                    alpha = true,
-                    barbar = true,
-                    flash = true,
-                    markdown = true,
-                    neogit = true,
-                    treesitter_context = true,
-                    telescope = {
-                        enabled = true,
-                        style = "nvchad",
-                    },
-                    which_key = true,
-                },
-            })
-            vim.cmd.colorscheme("catppuccin-frappe")
-        end,
-    },
-    {
-        "nvim-tree/nvim-web-devicons",
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				custom_highlights = function(colors)
+					return {
+						WhichKeyBorder = { fg = colors.base },
+						CmpBorder = { fg = colors.surface2 },
+						Pmenu = { link = "NormalFloat" },
+					}
+				end,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					treesitter = true,
+					alpha = true,
+					barbar = true,
+					flash = true,
+					markdown = true,
+					neogit = true,
+					treesitter_context = true,
+					which_key = true,
+                    mason = true,
+				},
+			})
+			vim.cmd.colorscheme("catppuccin-frappe")
+		end,
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
 }
