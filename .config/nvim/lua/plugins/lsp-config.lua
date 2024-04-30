2985dc2c2cd492c822a510709c942a39c8db4bda
Junior Sundar <junior.sundar@tii.ae>
2024-04-30 16:28:17 +0400

    Shiet
    Former-commit-id: a06bf506e37ea6ab716284fc1daf00406daa5e01

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,229 +1,230 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "pyright",
-                    "jedi_language_server",
-                    "clangd",
-                    "rust_analyzer",
-                    "gopls",
-                    "markdown_oxide",
-                    "marksman",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = { "hrsh7th/cmp-nvim-lsp" },
-        config = function()
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"jedi_language_server",
+					"clangd",
+					"rust_analyzer",
+					"gopls",
+					"markdown_oxide",
+					"marksman",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-            capabilities.workspace = {
-                didChangeWatchedFiles = {
-                    dynamicRegistration = true,
-                },
-            }
+			capabilities.workspace = {
+				didChangeWatchedFiles = {
+					dynamicRegistration = true,
+				},
+			}
 
-            local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-            local function lspSymbol(name, icon)
-                local hl = "DiagnosticSign" .. name
-                vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-            end
+			local function lspSymbol(name, icon)
+				local hl = "DiagnosticSign" .. name
+				vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+			end
 
-            lspSymbol("Error", "󰅙")
-            lspSymbol("Info", "󰋼")
-            lspSymbol("Hint", "󰌵")
-            lspSymbol("Warn", "")
+			lspSymbol("Error", "󰅙")
+			lspSymbol("Info", "󰋼")
+			lspSymbol("Hint", "󰌵")
+			lspSymbol("Warn", "")
 
-            vim.diagnostic.config({
-                virtual_text = {
-                    prefix = "",
-                },
-                signs = true,
-                underline = true,
-                update_in_insert = false,
-            })
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "",
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+			})
 
-            --  LspInfo window borders
-            local win = require("lspconfig.ui.windows")
-            local _default_opts = win.default_opts
+			--  LspInfo window borders
+			local win = require("lspconfig.ui.windows")
+			local _default_opts = win.default_opts
 
-            win.default_opts = function(options)
-                local opts = _default_opts(options)
-                opts.border = "single"
-                return opts
-            end
+			win.default_opts = function(options)
+				local opts = _default_opts(options)
+				opts.border = "single"
+				return opts
+			end
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        runtime = { version = "LuaJIT" },
-                        workspace = {
-                            checkThirdParty = false,
-                            library = {
-                                "${3rd}/luv/library",
-                                unpack(vim.api.nvim_get_runtime_file("", true)),
-                            },
-                        },
-                        completion = {
-                            callSnippet = "Replace",
-                        },
-                        hint = {
-                            enable = true,
-                        },
-                        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                        -- diagnostics = { disable = { 'missing-fields' } },
-                    },
-                },
-            })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								"${3rd}/luv/library",
+								unpack(vim.api.nvim_get_runtime_file("", true)),
+							},
+						},
+						completion = {
+							callSnippet = "Replace",
+						},
+						hint = {
+							enable = true,
+						},
+						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+						-- diagnostics = { disable = { 'missing-fields' } },
+					},
+				},
+			})
 
-            lspconfig.jedi_language_server.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.jedi_language_server.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-                settings = {
-                    python = {
-                        analysis = {
-                            autoSearchPaths = true,
-                            diagnosticMode = "openFilesOnly",
-                            useLibraryCodeForTypes = true,
-                        },
-                    },
-                },
-            })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				settings = {
+					python = {
+						analysis = {
+							autoSearchPaths = true,
+							diagnosticMode = "openFilesOnly",
+							useLibraryCodeForTypes = true,
+						},
+					},
+				},
+			})
 
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-                root_dir = lspconfig.util.root_pattern("compile_commands.json", ".clangd"),
-            })
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+				root_dir = lspconfig.util.root_pattern("compile_commands.json", ".clangd", ".git"),
+                cmd =  {'clangd', '--query-driver=/usr/bin/g++', '--compile-commands-dir='..vim.fn.getcwd()},
+			})
 
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-                settings = {
-                    gopls = {
-                        ["ui.inlayhint.hints"] = {
-                            assignVariableTypes = true,
-                            compositeLiteralFields = true,
-                            compositeLiteralTypes = true,
-                            constantValues = true,
-                            functionTypeParameters = true,
-                            parameterNames = true,
-                            rangeVariableTypes = true,
-                        },
-                    },
-                },
-            })
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				settings = {
+					gopls = {
+						["ui.inlayhint.hints"] = {
+							assignVariableTypes = true,
+							compositeLiteralFields = true,
+							compositeLiteralTypes = true,
+							constantValues = true,
+							functionTypeParameters = true,
+							parameterNames = true,
+							rangeVariableTypes = true,
+						},
+					},
+				},
+			})
 
-            lspconfig.markdown_oxide.setup({
-                capabilities = capabilities,
-                on_attach = function(_, bufnr)
-                    -- refresh codelens on TextChanged and InsertLeave as well
-                    vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave", "CursorHold", "LspAttach" }, {
-                        buffer = bufnr,
-                        callback = vim.lsp.codelens.refresh,
-                    })
-                    -- trigger codelens refresh
-                    vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
-                end,
-            })
+			lspconfig.markdown_oxide.setup({
+				capabilities = capabilities,
+				on_attach = function(_, bufnr)
+					-- refresh codelens on TextChanged and InsertLeave as well
+					vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave", "CursorHold", "LspAttach" }, {
+						buffer = bufnr,
+						callback = vim.lsp.codelens.refresh,
+					})
+					-- trigger codelens refresh
+					vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
+				end,
+			})
 
-            lspconfig.marksman.setup({
-                single_file_support = false,
-            })
+			lspconfig.marksman.setup({
+				single_file_support = false,
+			})
 
-            -- Use LspAttach autocommand to only map the following keys
-            -- after the language server attaches to the current buffer
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-                callback = function(event)
-                    -- Enable completion triggered by <c-x><c-o>
-                    vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+			-- Use LspAttach autocommand to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(event)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-                    -- Buffer local mappings.
-                    -- See `:help vim.lsp.*` for documentation on any of the below functions
-                    -- local opts = { buffer = event.buf }
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.server_capabilities.documentHighlightProvider then
-                        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                            buffer = event.buf,
-                            callback = vim.lsp.buf.document_highlight,
-                        })
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					-- local opts = { buffer = event.buf }
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.server_capabilities.documentHighlightProvider then
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.document_highlight,
+						})
 
-                        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                            buffer = event.buf,
-                            callback = vim.lsp.buf.clear_references,
-                        })
-                        vim.keymap.del("n", "K", { buffer = event.buf })
-                    end
-                end,
-            })
-        end,
-    },
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            local null_ls = require("null-ls")
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.clear_references,
+						})
+						vim.keymap.del("n", "K", { buffer = event.buf })
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
 
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.gofumpt,
-                },
-            })
-        end,
-    },
-    {
-        "nvimdev/lspsaga.nvim",
-        event = "LspAttach",
-        config = function()
-            require("lspsaga").setup({
-                lightbulb = {
-                    sign = false,
-                    virtual_text = true,
-                },
-                symbol_in_winbar = {
-                    enable = false,
-                },
-                ui = {
-                    -- currently only round theme
-                    theme = "rounded",
-                    -- border type can be single,double,rounded,solid,shadow.
-                    border = "solid",
-                    expand = "",
-                    collapse = "",
-                    preview = " ",
-                    code_action = " ",
-                    diagnostic = " ",
-                    incoming = " ",
-                    outgoing = " ",
-                    lines = { "┗", "┣", "┃", "━", "┏" },
-                    kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
-                },
-            })
-        end,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter", -- optional
-            "nvim-tree/nvim-web-devicons", -- optional
-        },
-    },
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.gofumpt,
+				},
+			})
+		end,
+	},
+	{
+		"nvimdev/lspsaga.nvim",
+		event = "LspAttach",
+		config = function()
+			require("lspsaga").setup({
+				lightbulb = {
+					sign = false,
+					virtual_text = true,
+				},
+				symbol_in_winbar = {
+					enable = false,
+				},
+				ui = {
+					-- currently only round theme
+					theme = "rounded",
+					-- border type can be single,double,rounded,solid,shadow.
+					border = "solid",
+					expand = "",
+					collapse = "",
+					preview = " ",
+					code_action = " ",
+					diagnostic = " ",
+					incoming = " ",
+					outgoing = " ",
+					lines = { "┗", "┣", "┃", "━", "┏" },
+					kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
+				},
+			})
+		end,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter", -- optional
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	},
 }
