9b34134b8a53d97274f2a9b614683714dd0bd53a
Junior Sundar <juniorsundar@gmail.com>
2024-05-05 02:15:56 +0400

    Updates
    Former-commit-id: 204a3a0d14d81cd185af6ea8aa8710608af11e3a

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,23 +1,23 @@
 return {
-	{
-		"echasnovski/mini.surround",
-		event = "VeryLazy",
-		config = function()
-			require("mini.surround").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.pairs",
-		event = "VeryLazy",
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.indentscope",
-		event = "VeryLazy",
-		config = function()
-			require("mini.indentscope").setup()
-		end,
-	},
+    {
+        "echasnovski/mini.surround",
+        event = "VeryLazy",
+        config = function()
+            require("mini.surround").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.pairs",
+        event = "VeryLazy",
+        config = function()
+            require("mini.pairs").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.ai",
+        event = "VeryLazy",
+        config = function()
+            require("mini.ai").setup()
+        end,
+    },
 }
