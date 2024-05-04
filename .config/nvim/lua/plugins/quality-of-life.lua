204a3a0d14d81cd185af6ea8aa8710608af11e3a
Junior Sundar <juniorsundar@gmail.com>
2024-05-05 02:15:56 +0400

    Updates

--- .config/nvim/lua/plugins/quality-of-life.lua
+++ .config/nvim/lua/plugins/quality-of-life.lua
@@ -3,28 +3,75 @@ return {
 		"folke/flash.nvim",
 		event = "VeryLazy",
 		opts = {},
-        -- stylua: ignore
-        keys = {
-            { "<C-s>",   mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-            { "<C-S-s>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-            { "r",       mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-            { "R",       mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<C-s>",   mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-        },
+		keys = {
+			{
+				"<C-s>",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"<C-S-s>",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<C-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
 	},
 	{
-		"ecthelionvi/NeoComposer.nvim",
-		dependencies = { "kkharji/sqlite.lua" },
-		config = function()
-			require("NeoComposer").setup({
-				window = {
-					winhl = {
-						Normal = "Normal",
-					},
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			indent = {
+				char = "╎",
+				tab_char = "╎",
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+					"norg",
 				},
-			})
-			require("NeoComposer.ui").status_recording()
-		end,
+			},
+		},
+		main = "ibl",
 	},
 	{
 		"mbbill/undotree",
--- .config/nvim/lua/plugins/statuslines.lua
+++ .config/nvim/lua/plugins/statuslines.lua
@@ -1,67 +1,67 @@
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        config = function()
-            local lualine_status, lualine = pcall(require, "lualine")
-            if not lualine_status then
-                return
-            end
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			local lualine_status, lualine = pcall(require, "lualine")
+			if not lualine_status then
+				return
+			end
 
-            local prose_status, prose = pcall(require, "nvim-prose")
-            if not prose_status then
-                return
-            end
+			local prose_status, prose = pcall(require, "nvim-prose")
+			if not prose_status then
+				return
+			end
 
-            -- configure Lualine with modified theme
-            lualine.setup({
-                options = {
-                    icons_enabled = true,
-                    theme = "auto",
-                    component_separators = { left = "", right = "" },
-                    -- section_separators = { left = "", right = "" },
-                    section_separators = { left = "", right = "" },
-                    disabled_filetypes = {
-                        "neo-tree",
-                        "no-neck-pain",
-                        "NvimTree",
-                        "oil",
-                    },
-                    ignore_focus = {},
-                    always_divide_middle = true,
-                    globalstatus = false,
-                    refresh = {
-                        statusline = 1000,
-                        tabline = 1000,
-                        winbar = 1000,
-                    },
-                },
-                sections = {
-                    lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-                    lualine_b = { "branch", "diagnostics" },
-                    lualine_c = { { "filename" } },
-                    lualine_x = { require("NeoComposer.ui").status_recording },
-                    lualine_y = { "filetype" },
-                    lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
-                },
-                inactive_sections = {
-                    lualine_c = { { "filename", path = 2 } },
-                    lualine_z = { "location" },
-                },
-                tabline = {},
-                winbar = {
-                    lualine_z = {
-                        {
-                            prose.word_count,
-                            cond = prose.is_available,
-                            separator = { left = "", right = "" },
-                            left_padding = 2,
-                            right_padding = 2,
-                        },
-                    },
-                },
-                inactive_winbar = {},
-                extensions = {},
-            })
-        end,
-    },
+			-- configure Lualine with modified theme
+			lualine.setup({
+				options = {
+					icons_enabled = true,
+					theme = "auto",
+					component_separators = { left = "", right = "" },
+					-- section_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						"neo-tree",
+						"no-neck-pain",
+						"NvimTree",
+						"oil",
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					},
+				},
+				sections = {
+					lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+					lualine_b = { "branch", "diagnostics" },
+					lualine_c = { { "filename" } },
+					lualine_x = {},
+					lualine_y = { "filetype" },
+					lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
+				},
+				inactive_sections = {
+					lualine_c = { { "filename", path = 2 } },
+					lualine_z = { "location" },
+				},
+				tabline = {},
+				winbar = {
+					lualine_z = {
+						{
+							prose.word_count,
+							cond = prose.is_available,
+							separator = { left = "", right = "" },
+							left_padding = 2,
+							right_padding = 2,
+						},
+					},
+				},
+				inactive_winbar = {},
+				extensions = {},
+			})
+		end,
+	},
 }
