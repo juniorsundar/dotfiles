1ee4fb8f4dbc6c515596d83fd727b306f6d5d4b9
Junior Sundar <juniorsundar@gmail.com>
2024-05-05 14:15:23 +0400

    Git fixed in nvim

--- .config/nvim/lua/plugins/quality-of-life.lua
+++ .config/nvim/lua/plugins/quality-of-life.lua
@@ -1,104 +1,106 @@
 return {
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{
-				"<C-s>",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-			{
-				"<C-S-s>",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter",
-			},
-			{
-				"r",
-				mode = "o",
-				function()
-					require("flash").remote()
-				end,
-				desc = "Remote Flash",
-			},
-			{
-				"R",
-				mode = { "o", "x" },
-				function()
-					require("flash").treesitter_search()
-				end,
-				desc = "Tresitter Search",
-			},
-			{
-				"<C-s>",
-				mode = { "c" },
-				function()
-					require("flash").toggle()
-				end,
-				desc = "Toggle Flash Search",
-			},
-		},
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		opts = {
-			indent = {
-				char = "╎",
-				tab_char = "╎",
-			},
-			scope = { enabled = false },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-					"norg",
-				},
-			},
-		},
-		main = "ibl",
-	},
-	{
-		"jbyuki/nabla.nvim",
-		ft = { "markdown", "norg" },
-		keys = {
-			{ "<S-E>", ':lua require"nabla".toggle_virt()<cr>', "toggle equations" },
-			{ "<S-H>", ':lua require"nabla".popup()<cr>', "hover equation" },
-		},
-	},
-	{
-		"skwee357/nvim-prose",
-		ft = { "markdown", "txt", "norg" },
-		config = function()
-			require("nvim-prose").setup({
-				wpm = 200.0,
-				filetypes = { "markdown", "asciidoc", "norg" },
-				placeholders = {
-					words = "words",
-					minutes = "min",
-				},
-			})
-		end,
-	},
-	{
-		"mbbill/undotree",
-	},
-	{
-		"tpope/vim-surround",
-	},
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "<C-s>",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").jump()
+                end,
+                desc = "Flash",
+            },
+            {
+                "<C-S-s>",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").treesitter()
+                end,
+                desc = "Flash Treesitter",
+            },
+            {
+                "r",
+                mode = "o",
+                function()
+                    require("flash").remote()
+                end,
+                desc = "Remote Flash",
+            },
+            {
+                "R",
+                mode = { "o", "x" },
+                function()
+                    require("flash").treesitter_search()
+                end,
+                desc = "Tresitter Search",
+            },
+            {
+                "<C-s>",
+                mode = { "c" },
+                function()
+                    require("flash").toggle()
+                end,
+                desc = "Toggle Flash Search",
+            },
+        },
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = {
+            indent = {
+                char = "╎",
+                tab_char = "╎",
+            },
+            scope = { enabled = false },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                    "norg",
+                    "git",
+                    "fugitive",
+                },
+            },
+        },
+        main = "ibl",
+    },
+    {
+        "jbyuki/nabla.nvim",
+        ft = { "markdown", "norg" },
+        keys = {
+            { "<S-E>", ':lua require"nabla".toggle_virt()<cr>', "toggle equations" },
+            { "<S-H>", ':lua require"nabla".popup()<cr>',       "hover equation" },
+        },
+    },
+    {
+        "skwee357/nvim-prose",
+        ft = { "markdown", "txt", "norg" },
+        config = function()
+            require("nvim-prose").setup({
+                wpm = 200.0,
+                filetypes = { "markdown", "asciidoc", "norg" },
+                placeholders = {
+                    words = "words",
+                    minutes = "min",
+                },
+            })
+        end,
+    },
+    {
+        "mbbill/undotree",
+    },
+    {
+        "tpope/vim-surround",
+    },
 }
