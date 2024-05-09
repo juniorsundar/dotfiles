789516caa1c64bf7605decf66ac92b54fcc3e2ca
Junior Sundar <junior.sundar@tii.ae>
2024-05-09 20:35:41 +0400

    Backup: 20240426K1628

--- .config/nvim/lua/plugins/notifications.lua
+++ .config/nvim/lua/plugins/notifications.lua
@@ -20,15 +20,15 @@ return {
 						-- icon_hl_group: optional hl_group for the icon
 						-- title: set to anything or empty string to hide
 						cmdline = { pattern = "^:", icon = "", lang = "vim" },
-						search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-						search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+						search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex", view = "cmdline" },
+						search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex", view = "cmdline"},
 						filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
 						lua = {
 							pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" },
 							icon = "",
 							lang = "lua",
 						},
-						help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+						help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
 						input = {}, -- Used by input()
 						-- lua = false, -- to disable a format, set to `false`
 					},
@@ -195,7 +195,7 @@ return {
 					-- you can also add custom presets that you can enable/disable with enabled=true
 					bottom_search = false, -- use a classic bottom cmdline for search
 					command_palette = false, -- position the cmdline and popupmenu together
-					long_message_to_split = false, -- long messages will be sent to a split
+					long_message_to_split = true, -- long messages will be sent to a split
 					inc_rename = false, -- enables an input dialog for inc-rename.nvim
 					lsp_doc_border = false, -- add a border to hover docs and signature help
 				},
