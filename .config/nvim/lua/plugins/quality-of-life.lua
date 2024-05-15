60c6cb29172b9e1f3386effa6925f18962f1b014
Junior Sundar <juniorsundar@gmail.com>
2024-05-15 23:53:53 +0400

    Removed word count it is unnecessary

--- .config/nvim/lua/plugins/quality-of-life.lua
+++ .config/nvim/lua/plugins/quality-of-life.lua
@@ -76,20 +76,6 @@ return {
 		main = "ibl",
 	},
 	{
-		"skwee357/nvim-prose",
-		ft = { "markdown", "txt", "norg" },
-		config = function()
-			require("nvim-prose").setup({
-				wpm = 200.0,
-				filetypes = { "markdown", "asciidoc", "norg" },
-				placeholders = {
-					words = "words",
-					minutes = "min",
-				},
-			})
-		end,
-	},
-	{
 		"mbbill/undotree",
         cmd = "UndotreeToggle"
 	},
