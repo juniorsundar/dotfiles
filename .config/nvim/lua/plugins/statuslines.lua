84c4588645b2a08bf9c5c6831f895e72caee1fcb
Junior Sundar <junior.sundar@tii.ae>
2024-05-16 10:50:50 +0400

    Updated lazy lock

--- .config/nvim/lua/plugins/statuslines.lua
+++ .config/nvim/lua/plugins/statuslines.lua
@@ -111,7 +111,6 @@ return {
 					return conditions.buffer_matches({ buftype = { "terminal" } })
 				end,
 				hl = { bg = "bg" },
-				-- Quickly add a condition to the ViMode to only show it when buffer is active!
 				{ condition = conditions.is_active, heirline_components.component.mode({ mode_text = {} }) },
 				heirline_components.component.fill(),
 				TerminalName,
@@ -150,80 +149,4 @@ return {
 			heirline.setup(opts)
 		end,
 	},
-	-- {
-	-- 	"nvim-lualine/lualine.nvim",
-	-- 	config = function()
-	-- 		local lualine_status, lualine = pcall(require, "lualine")
-	-- 		if not lualine_status then
-	-- 			return
-	-- 		end
-	--
-	-- 		local prose_status, prose = pcall(require, "nvim-prose")
-	-- 		if not prose_status then
-	-- 			return
-	-- 		end
-	--
-	-- 		-- configure Lualine with modified theme
-	-- 		lualine.setup({
-	-- 			options = {
-	-- 				icons_enabled = true,
-	-- 				theme = "auto",
-	-- 				component_separators = { left = "", right = "" },
-	-- 				section_separators = { left = "", right = "" },
-	-- 				disabled_filetypes = {
-	--                        "alpha",
-	--                        "diff",
-	--                        "DiffviewFiles",
-	--                        "NeogitCommitMessage",
-	--                        "NeogitDiffView",
-	-- 					"neo-tree",
-	-- 					"no-neck-pain",
-	-- 					"NvimTree",
-	-- 					"oil",
-	--                        "undotree",
-	-- 				},
-	-- 				ignore_focus = {},
-	-- 				always_divide_middle = true,
-	-- 				globalstatus = false,
-	-- 				refresh = {
-	-- 					statusline = 1000,
-	-- 					tabline = 1000,
-	-- 					winbar = 1000,
-	-- 				},
-	-- 			},
-	-- 			sections = {
-	-- 				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-	-- 				lualine_b = { "branch", "diagnostics" },
-	-- 				lualine_c = { { "filename" } },
-	-- 				lualine_x = {
-	-- 					{
-	-- 						require("noice").api.statusline.mode.get,
-	-- 						cond = require("noice").api.statusline.mode.has,
-	-- 						color = { fg = "#ff9e64" },
-	-- 					},
-	-- 				},
-	-- 				lualine_y = { "filetype" },
-	-- 				lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
-	-- 			},
-	-- 			inactive_sections = {
-	-- 				lualine_c = { { "filename", path = 2 } },
-	-- 				lualine_z = { "location" },
-	-- 			},
-	-- 			tabline = {},
-	-- 			winbar = {
-	-- 				lualine_z = {
-	-- 					{
-	-- 						prose.word_count,
-	-- 						cond = prose.is_available,
-	-- 						separator = { left = "", right = "" },
-	-- 						left_padding = 2,
-	-- 						right_padding = 2,
-	-- 					},
-	-- 				},
-	-- 			},
-	-- 			inactive_winbar = {},
-	-- 			extensions = {},
-	-- 		})
-	-- 	end,
-	-- },
 }
