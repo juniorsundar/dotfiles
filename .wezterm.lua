106e37d6673a47cb076f44cfb14519b8fd788769
Junior Sundar <juniorsundar@gmail.com>
2024-05-18 00:30:09 +0400

    Updated wezterm confi
    Former-commit-id: 5853dbe8a77d433b693623de339d4a67da03dbf8

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,7 +15,7 @@ end
 
 -- For example, changing the color scheme:
 -- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Light" })
-config.font = wezterm.font("FiraCode Nerd Font Ret", { weight = "Regular" })
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" })
 -- config.font = wezterm.font("CaskaydiaCove NF", { weight = "Regular" })
 -- config.font = wezterm.font("VictorMono NF", { weight = "Regular" })
 config.font_size = 11
