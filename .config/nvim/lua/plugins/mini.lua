a30fff7ed1634f1e59fa6ff42fe7b31ae269e837
Junior Sundar <juniorsundar@gmail.com>
2024-05-27 22:51:10 +0400

    Updated some bs

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,11 +1,4 @@
 return {
-    -- {
-    --     "echasnovski/mini.surround",
-    --     event = "VeryLazy",
-    --     config = function()
-    --         require("mini.surround").setup()
-    --     end,
-    -- },
     {
         "echasnovski/mini.pairs",
         event = "VeryLazy",
