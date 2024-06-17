f4d884ab845cf2c228290d99b1e689225a066765
Junior Sundar <juniorsundar@gmail.com>
2024-06-17 16:45:15 +0400

    Fixed wrapping in Neorg
    Former-commit-id: 57d3084ebe48d882eadd9b174acd9af23ef110e5

---
+++ .config/nvim/ftplugin/norg.lua
@@ -0,0 +1,33 @@
+vim.api.nvim_create_autocmd("InsertEnter", {
+	pattern = "*.norg",
+	group = vim.api.nvim_create_augroup("NeorgWrapOn", { clear = true }),
+	callback = function(_)
+		vim.cmd("set wrap")
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*.norg",
+	group = vim.api.nvim_create_augroup("NeorgWrapOff", { clear = true }),
+	callback = function(_)
+		vim.cmd("set nowrap")
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*.norg",
+	group = vim.api.nvim_create_augroup("NeorgWrapOffBuf", { clear = true }),
+	callback = function(_)
+		vim.cmd("set nowrap")
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+	pattern = "*.norg",
+	group = vim.api.nvim_create_augroup("NeorgWrapOnBuf", { clear = true }),
+	callback = function(_)
+		vim.cmd("set wrap")
+	end,
+})
+
+vim.opt.scrolloff = 999
