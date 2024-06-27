49937b0a61417e7beb5741cbd31f39ee8bbe4d12
Junior Sundar <juniorsundar@gmail.com>
2024-06-27 23:59:50 +0400

    Updated zellij

--- .wezterm.lua
+++ .wezterm.lua
@@ -29,6 +29,13 @@ config.hide_tab_bar_if_only_one_tab = true
 -- Disable font ligatures
 -- config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
+-- config.window_padding = {
+-- 	left = 0,
+-- 	right = 0,
+-- 	top = 0,
+-- 	bottom = 0,
+-- }
+
 config.keys = {
 	{
 		key = "t",
