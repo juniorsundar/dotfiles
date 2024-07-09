e0493774d89e9ec968bcb00399f31c33c6d3e168
Junior Sundar <juniorsundar@gmail.com>
2024-07-10 00:05:12 +0400

    Updated polybar script

--- .config/polybar/scripts/get_window_info.sh
+++ .config/polybar/scripts/get_window_info.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 # Get the active window ID
-window_id=$(xdotool getactivewindow)
+window_id=$(xdotool getactivewindow 2>/dev/null)
+if [ $? -ne 0 ]; then
+  echo ""
+  exit 1
+fi
 
 # Get the application name
-app_name=$(xprop -id "$window_id" | grep WM_CLASS | awk -F '"' '{print $4}')
+app_name=$(xprop -id "$window_id" 2>/dev/null | grep WM_CLASS | awk -F '"' '{print $4}')
+if [ $? -ne 0 ]; then
+  echo ""
+  exit 1
+fi
 
 # Get the window title
-window_title=$(xdotool getwindowname "$window_id")
+window_title=$(xdotool getwindowname "$window_id" 2>/dev/null)
+if [ $? -ne 0 ]; then
+  echo ""
+  exit 1
+fi
 
 # Print in the desired format
 echo "$app_name | $window_title"
