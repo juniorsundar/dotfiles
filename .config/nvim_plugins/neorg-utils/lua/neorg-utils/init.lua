0d92c846df1762ef4ae183c0e219ae39a4f21ca1
Junior Sundar <junior.sundar@tii.ae>
2024-08-09 00:48:55 +0400

    Moved neorg-utils to a plugin
    Former-commit-id: 4d4c5510302de0e3ed2c5406b5b9ea26ab29d2c9

--- .config/nvim_plugins/neorg-utils/lua/neorg-utils/init.lua
+++
@@ -1,9 +0,0 @@
-local telescopic = require("neorg-utils.telescopic")
-local agenda = require("neorg-utils.neorg-agenda")
-local utils = require("neorg-utils.utils")
-
-return {
-    telescopic = telescopic,
-    agenda = agenda,
-    utils = utils
-}
