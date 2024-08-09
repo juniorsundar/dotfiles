036b977b51a5a815f89c3b9a4994dd686c5a1578
Junior Sundar <junior.sundar@tii.ae>
2024-08-09 14:29:12 +0400

    Updated plugin stuff
    Former-commit-id: 019d452166f4ca7a999522f22759adc6f3060942

--- .screenlayout/home_office.sh
+++ .screenlayout/home_office.sh
@@ -3,5 +3,7 @@ xrandr --output eDP-1 --mode 1920x1200 --pos 0x240 --rotate normal --output HDMI
 
 sleep 2
 killall i3bar
+killall polybar
 nitrogen --restore
 setxkbmap -layout us -option grp:shifts_toggle
+bash ~/.config/polybar/launch_polybar.sh
