aafd1f58ccf9c9f4ee9809844adbd4199d74e16d
Junior Sundar <juniorsundar@gmail.com>
2024-08-29 22:08:36 +0400

    Cleaned fonts
    Brought back Fira Code
    
    New release for neorg_extras
    
    Updated keymaps
    
    Updated fish fzf config
    
    Updated fzf keybinds in fish
    
    Update fish
    
    Updated screenlayout for office
    
    Updated polybar
    
    Updated default terminal to kitty
    
    Update kitty
    
    Updated neorg_extras
    
    Updated thems
    
    Changed theme file
    
    Update kitty for tmux instead
    
    Update wezterm to use zsh by default

--- .bashrc
+++ .bashrc
@@ -149,3 +149,7 @@ eval "$(starship init bash)"
 eval "$(zoxide init bash)"
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- .wezterm.lua
+++ .wezterm.lua
@@ -20,6 +20,7 @@ config.font = wezterm.font_with_fallback({
     -- { family = "Iosevka", weight = "Regular" },
     { family = "Symbols Nerd Font", weight = "Regular" },
 })
+
 config.font_size = 13
 
 config.colors = {
@@ -50,7 +51,6 @@ config.hide_tab_bar_if_only_one_tab = true
 -- config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 -- Spawn a fish shell in login mode
 
-config.default_prog = { '/usr/bin/fish'}
 config.window_padding = {
     -- 	left = 2.5,
     -- 	right = 2.5,
