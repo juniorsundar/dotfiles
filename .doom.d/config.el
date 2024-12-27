240f74a1f98cb5fe48acd715fd2d162ddbaaf54f
Junior Sundar <junior.sundar@tii.ae>
2024-12-27 13:57:28 +0400

    Updated admonitions

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,26 +21,48 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 16 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 17))
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 18 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 19))
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (after! emacs
   (setq display-line-numbers-type 'relative)
   )
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (after! org
   (setq org-todo-keywords
         '((sequence "TODO(t)" "DOING(d!)" "HOLD(h)" "|" "DONE(D)" "CANCELLED(c)" "MAYBE(m)")))
@@ -74,7 +96,8 @@
     (let ((overlay (make-overlay start end)))
       (overlay-put overlay 'face face)
       (overlay-put overlay 'line-prefix
-                   (propertize " " 'face face 'display '(space :align-to 0)))
+                   nil)
+                   ;; (propertize " " 'face face 'display '(space :align-to 0)))
       (overlay-put overlay 'after-string
                    (propertize " " 'face face 'display '(space :align-to right-margin)))))
   (defun highlight-org-block-region ()
@@ -103,33 +126,6 @@
       (remove-overlays (point-min) (point-max))))
   (add-hook 'org-mode-hook 'org-block-highlight-mode)
 
-  ;; ;; Define custom faces for the blocks and their content
-  ;; (defface my-note-face
-  ;;   '((t (:background "#a6d189" :foreground "#1e1e2e" :weight bold)))
-  ;;   "Face for notes.")
-
-  ;; (defface my-important-face
-  ;;   '((t (:background "#ea6962" :foreground "#1e1e2e" :weight bold)))
-  ;;   "Face for important notes.")
-
-  ;; (defface my-warning-face
-  ;;   '((t (:background "#e5c890" :foreground "#1e1e2e" :weight bold)))
-  ;;   "Face for warnings.")
-  ;; (defun apply-custom-org-faces ()
-  ;;   "Apply custom faces for specific Org blocks, highlighting the entire width."
-  ;;   (font-lock-add-keywords
-  ;;    nil
-  ;;    '(("^[ \t]*#\\+BEGIN_NOTE\\([^\n]*\n\\)\\(?:.*\n\\)*?[ \t]*#\\+END_NOTE.*$"
-  ;;       (0 'my-note-face t))
-  ;;      ("^[ \t]*#\\+BEGIN_IMPORTANT\\([^\n]*\n\\)\\(?:.*\n\\)*?[ \t]*#\\+END_IMPORTANT.*$"
-  ;;       (0 'my-important-face t))
-  ;;      ("^[ \t]*#\\+BEGIN_WARN\\([^\n]*\n\\)\\(?:.*\n\\)*?[ \t]*#\\+END_WARN.*$"
-  ;;       (0 'my-warning-face t)))
-  ;;    'append))
-
-  ;; ;; Add the function to org-mode-hook
-  ;; (add-hook 'org-mode-hook 'apply-custom-org-faces)
-  ;; Ensure the changes take effect
   (font-lock-mode 1)
 
   (setq org-directory "~/Dropbox/org")
@@ -171,38 +167,6 @@
   )
 (evil-define-key 'normal 'global (kbd "C-w") evil-window-map)
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-;; (setq doom-theme 'catppuccin)
 (load-theme 'catppuccin :no-confirm)
 (setq catppuccin-flavor 'cyberdream) ;; or 'latte, 'macchiato, or 'mocha
 (catppuccin-reload)
