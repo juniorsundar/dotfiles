5836c390227adb998426967414770538880239a2
Junior Sundar <juniorsundar@gmail.com>
2025-07-16 22:42:05 +0400

    Added some fonts

--- wezterm/.config/wezterm/fonts/caskaydiacove.lua
+++ wezterm/.config/wezterm/fonts/caskaydiacove.lua
@@ -1,24 +1,8 @@
 M = {
     apply_font = function(config, wezterm)
-        config.font = wezterm.font("CaskaydiaCove NF", { weight = "Regular" })
-        config.font_rules = {
-            {
-                intensity = "Bold",
-                italic = false,
-                font = wezterm.font("CaskaydiaCove NF", { weight = "Bold" }),
-            },
-            {
-                intensity = "Normal",
-                italic = true,
-                font = wezterm.font("CaskaydiaCove NF", { weight = "Regular", style = "Italic" }),
-            },
-            {
-                intensity = "Bold",
-                italic = true,
-                font = wezterm.font("CaskaydiaCove NF", { weight = "Bold", style = "Italic" }),
-            },
+        config.font = wezterm.font_with_fallback {
+            'CaskaydiaCove NF',
         }
-        config.line_height = 1.1
         return config
     end
 }
