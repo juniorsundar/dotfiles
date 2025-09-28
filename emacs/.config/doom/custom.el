e50bf0a810018ccb251e18a46473dd650a63bea4
Junior Sundar <juniorsundar@gmail.com>
2025-09-28 21:37:18 +0400

    Doom one theme

---
+++ emacs/.config/doom/custom.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit bold :height 1.3))))
+ '(org-level-1 ((t (:inherit bold :height 1.3))))
+ '(org-level-2 ((t (:inherit bold :height 1.2))))
+ '(org-level-3 ((t (:weight normal :height 1.1)))))
